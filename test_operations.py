19957e52135806cb179bc66019afa835e40b0059
KofiKarikariBonsuJr <kkb@njit.edu>
2025-11-04 06:43:00 -0500

    fix: changed test_operations to account for b and a variables being switched

--- test_operations.py
+++ test_operations.py
@@ -6,7 +6,7 @@ import pytest
 def test_add():
     assert addition(2, 3) == 5
 def test_subtract():
-    assert subtraction(5, 3) == 2
+    assert subtraction(3, 5) == 2
 def test_divide():
-    assert division(6, 2) == 3    
+    assert division(2, 6) == 3    
 
